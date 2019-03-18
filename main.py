6306f6bd87bb95a6918b15c98f5c1c2485f9d189
Szabó Gergő <Szabogergo1992@gmail.com>
2019-03-18 14:12:48 +0100

    initial changes, added user stories txt

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request,redirect
+from flask import Flask, render_template, request, redirect
 
 
 app = Flask(__name__)
@@ -11,4 +11,4 @@ def index():
 
 if __name__ == '__main__':
     app.run(debug=True,
-                port=7000)
+            port=7000)
