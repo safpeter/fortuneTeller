e396d478d26a0257c545fc96e3b5e8a4d0e3b6b1
ivanmatiz <ivanmatiz@gmail.com>
2019-03-19 14:31:45 +0100

    main page server ready

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 
 
 app = Flask(__name__)
 
+saved_data = {}
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method == "POST":
+        saved_data['sucker_name'] = request.form['sucker_name']
+    return render_template('index.html')\
 
+
+# @app.route('/prediction', methods=['GET', 'POST'])
+# def index():
+#     if request.method == "POST":
+#         saved_data['sucker_name'] = request.form['sucker_name']
+#         return redirect('/')
+#     return render_template('prediction.html')
+#
 
 if __name__ == '__main__':
     app.run(debug=True,
