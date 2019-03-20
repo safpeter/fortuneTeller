900e74780c8fc46acf2e4ea299d8f2399ed7f9cd
Sáfár Péter <safpeter03@gmail.com>
2019-03-20 11:33:01 +0100

    some basic changes

--- static/js/game.js
+++ static/js/game.js
@@ -1,13 +1,18 @@
 
 const card = document.getElementsByClassName('card');
 
-let count = 0
+let count = 0;
 
 for (let i = 0 ;i<card.length;i++) {
     card[i].addEventListener('click',function () {
         if (count < 3) {
             card[i].style.visibility = 'hidden';
             count++;
-        }})
+        }
+        else if (count ===3) {
+            card[i].removeEventListener()
+        }
+        })
 }
+
 
