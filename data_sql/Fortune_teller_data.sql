57de00bec3d91a8330b954fe21335aba41bddf6c
Szabó Gergő <Szabogergo1992@gmail.com>
2019-03-20 16:15:16 +0100

    card selection modified, html and js part reworked in correlation with the card selection

--- data_sql/Fortune_teller_data.sql
+++ data_sql/Fortune_teller_data.sql
@@ -10,14 +10,14 @@ ALTER TABLE IF EXISTS ONLY public.cards DROP CONSTRAINT IF EXISTS pk_card_id CAS
 
 DROP TABLE IF EXISTS public.question CASCADE;
 DROP SEQUENCE IF EXISTS public.question_id_seq;
-DROP TABLE IF EXISTS public.answer;
+DROP TABLE IF EXISTS public.answer CASCADE;
 DROP SEQUENCE IF EXISTS public.answer_id_seq;
-DROP TABLE IF EXISTS public.comment;
+DROP TABLE IF EXISTS public.comment CASCADE;
 DROP SEQUENCE IF EXISTS public.comment_id_seq;
-DROP TABLE IF EXISTS public.question_tag;
-DROP TABLE IF EXISTS public.tag;
+DROP TABLE IF EXISTS public.question_tag CASCADE;
+DROP TABLE IF EXISTS public.tag CASCADE;
 DROP SEQUENCE IF EXISTS public.tag_id_seq;
-DROP TABLE IF EXISTS public.users;
+DROP TABLE IF EXISTS public.users CASCADE;
 DROP SEQUENCE IF EXISTS public.users_id_seq;
 
 
@@ -37,51 +37,96 @@ ALTER TABLE ONLY cards
 
 
 
-INSERT INTO cards VALUES (1, 'baby', '', '+', 'gypsy_card/baby.png');
-INSERT INTO cards VALUES (2, 'consolation', '', '+', 'gypsy_card/consolation.png');
-INSERT INTO cards VALUES (3, 'death', '', '+', 'gypsy_card/death.png');
-INSERT INTO cards VALUES (4, 'despair', '', '+', 'gypsy_card/despair.png');
-INSERT INTO cards VALUES (5, 'doctor', '', '+', 'gypsy_card/doctor.png');
-INSERT INTO cards VALUES (6, 'enemy', '', '+', 'gypsy_card/enemy.png');
-INSERT INTO cards VALUES (7, 'faithfulness', '', '+', 'gypsy_card/faithfulness.png');
-INSERT INTO cards VALUES (8, 'falseness', '', '+', 'gypsy_card/falseness.png');
-INSERT INTO cards VALUES (9, 'foe', '', '+', 'gypsy_card/foe.png');
-INSERT INTO cards VALUES (10, 'fortune', '', '+', 'gypsy_card/fortune.png');
-INSERT INTO cards VALUES (11, 'gift', '', '+', 'gypsy_card/gift.png');
-INSERT INTO cards VALUES (12, 'haughtiness', '', '+', 'gypsy_card/haughtiness.png');
-INSERT INTO cards VALUES (13, 'hope', '', '+', 'gypsy_card/hope.png');
-INSERT INTO cards VALUES (14, 'house', '', '+', 'gypsy_card/house.png');
-INSERT INTO cards VALUES (15, 'journey', '', '+', 'gypsy_card/journey.png');
-INSERT INTO cards VALUES (16, 'joyfulness', '', '+', 'gypsy_card/joyfulness.png');
-INSERT INTO cards VALUES (17, 'letter', '', '+', 'gypsy_card/letter.png');
-INSERT INTO cards VALUES (18, 'lord', '', '+', 'gypsy_card/lord.png');
-INSERT INTO cards VALUES (19, 'love', '', '+', 'gypsy_card/love.png');
-INSERT INTO cards VALUES (20, 'lover', '', '+', 'gypsy_card/lover.png');
-INSERT INTO cards VALUES (21, 'malady', '', '+', 'gypsy_card/malady.png');
-INSERT INTO cards VALUES (22, 'melancholy', '', '+', 'gypsy_card/melancholy.png');
-INSERT INTO cards VALUES (23, 'messenger', '', '+', 'gypsy_card/messenger.png');
-INSERT INTO cards VALUES (24, 'misfortune', '', '+', 'gypsy_card/misfortune.png');
-INSERT INTO cards VALUES (25, 'money', '', '+', 'gypsy_card/money.png');
-INSERT INTO cards VALUES (26, 'old_woman', '', '+', 'gypsy_card/old_woman.png');
-INSERT INTO cards VALUES (27, 'party', '', '+', 'gypsy_card/party.png');
-INSERT INTO cards VALUES (28, 'pleasure-seekers', '', '+', 'gypsy_card/pleasure-seekers.png');
-INSERT INTO cards VALUES (29, 'priest', '', '+', 'gypsy_card/priest.png');
-INSERT INTO cards VALUES (30, 'prison', '', '+', 'gypsy_card/prison.png');
-INSERT INTO cards VALUES (31, 'reunion', '', '+', 'gypsy_card/reunion.png');
-INSERT INTO cards VALUES (32, 'scholar', '', '+', 'gypsy_card/scholar.png');
-INSERT INTO cards VALUES (33, 'servant', '', '+', 'gypsy_card/servant.png');
-INSERT INTO cards VALUES (34, 'service', '', '+', 'gypsy_card/service.png');
-INSERT INTO cards VALUES (35, 'sighs', '', '+', 'gypsy_card/sighs.png');
-INSERT INTO cards VALUES (36, 'soldier', '', '+', 'gypsy_card/soldier.png');
-INSERT INTO cards VALUES (37, 'sorrow', '', '+', 'gypsy_card/sorrow.png');
-INSERT INTO cards VALUES (38, 'sweetheart', '', '+', 'gypsy_card/sweetheart.png');
-INSERT INTO cards VALUES (39, 'thief', '', '+', 'gypsy_card/thief.png');
-INSERT INTO cards VALUES (40, 'thought', '', '+', 'gypsy_card/thought.png');
-INSERT INTO cards VALUES (41, 'visit', '', '+', 'gypsy_card/visit.png');
-INSERT INTO cards VALUES (42, 'waiting', '', '+', 'gypsy_card/waiting.png');
-INSERT INTO cards VALUES (43, 'wedding', '', '+', 'gypsy_card/wedding.png');
-INSERT INTO cards VALUES (44, 'widower', '', '+', 'gypsy_card/widower.png');
-INSERT INTO cards VALUES (45, 'wife', '', '+', 'gypsy_card/wife.png');
-INSERT INTO cards VALUES (46, 'young_woman', '', '+', 'gypsy_card/young_woman.png');
+INSERT INTO cards VALUES (1, 'baby', '', '+', '../static/gypsy_card/baby.png');
+
+INSERT INTO cards VALUES (2, 'consolation', '', '+', '../static/gypsy_card/consolation.png');
+
+INSERT INTO cards VALUES (3, 'death', '', '+', '../static/gypsy_card/death.png');
+
+INSERT INTO cards VALUES (4, 'despair', '', '+', '../static/gypsy_card/despair.png');
+
+INSERT INTO cards VALUES (5, 'doctor', '', '+', '../static/gypsy_card/doctor.png');
+
+INSERT INTO cards VALUES (6, 'enemy', '', '+', '../static/gypsy_card/enemy.png');
+
+INSERT INTO cards VALUES (7, 'faithfulness', '', '+', '../static/gypsy_card/faithfulness.png');
+
+INSERT INTO cards VALUES (8, 'falseness', '', '+', '../static/gypsy_card/falseness.png');
+
+INSERT INTO cards VALUES (9, 'foe', '', '+', '../static/gypsy_card/foe.png');
+
+INSERT INTO cards VALUES (10, 'fortune', '', '+', '../static/gypsy_card/fortune.png');
+
+INSERT INTO cards VALUES (11, 'gift', '', '+', '../static/gypsy_card/gift.png');
+
+INSERT INTO cards VALUES (12, 'haughtiness', '', '+', '../static/gypsy_card/haughtiness.png');
+
+INSERT INTO cards VALUES (13, 'hope', '', '+', '../static/gypsy_card/hope.png');
+
+INSERT INTO cards VALUES (14, 'house', '', '+', '../static/gypsy_card/house.png');
+
+INSERT INTO cards VALUES (15, 'journey', '', '+', '../static/gypsy_card/journey.png');
+
+INSERT INTO cards VALUES (16, 'joyfulness', '', '+', '../static/gypsy_card/joyfulness.png');
+
+INSERT INTO cards VALUES (17, 'letter', '', '+', '../static/gypsy_card/letter.png');
+
+INSERT INTO cards VALUES (18, 'lord', '', '+', '../static/gypsy_card/lord.png');
+
+INSERT INTO cards VALUES (19, 'love', '', '+', '../static/gypsy_card/love.png');
+
+INSERT INTO cards VALUES (20, 'lover', '', '+', '../static/gypsy_card/lover.png');
+
+INSERT INTO cards VALUES (21, 'malady', '', '+', '../static/gypsy_card/malady.png');
+
+INSERT INTO cards VALUES (22, 'melancholy', '', '+', '../static/gypsy_card/melancholy.png');
+
+INSERT INTO cards VALUES (23, 'messenger', '', '+', '../static/gypsy_card/messenger.png');
+
+INSERT INTO cards VALUES (24, 'misfortune', '', '+', '../static/gypsy_card/misfortune.png');
+
+INSERT INTO cards VALUES (25, 'money', '', '+', '../static/gypsy_card/money.png');
+
+INSERT INTO cards VALUES (26, 'old_woman', '', '+', '../static/gypsy_card/old_woman.png');
+
+INSERT INTO cards VALUES (27, 'party', '', '+', '../static/gypsy_card/party.png');
+
+INSERT INTO cards VALUES (28, 'pleasure-seekers', '', '+', '../static/gypsy_card/pleasure-seekers.png');
+
+INSERT INTO cards VALUES (29, 'priest', '', '+', '../static/gypsy_card/priest.png');
+
+INSERT INTO cards VALUES (30, 'prison', '', '+', '../static/gypsy_card/prison.png');
+
+INSERT INTO cards VALUES (31, 'reunion', '', '+', '../static/gypsy_card/reunion.png');
+
+INSERT INTO cards VALUES (32, 'scholar', '', '+', '../static/gypsy_card/scholar.png');
+
+INSERT INTO cards VALUES (33, 'servant', '', '+', '../static/gypsy_card/servant.png');
+
+INSERT INTO cards VALUES (34, 'service', '', '+', '../static/gypsy_card/service.png');
+
+INSERT INTO cards VALUES (35, 'sighs', '', '+', '../static/gypsy_card/sighs.png');
+
+INSERT INTO cards VALUES (36, 'soldier', '', '+', '../static/gypsy_card/soldier.png');
+
+INSERT INTO cards VALUES (37, 'sorrow', '', '+', '../static/gypsy_card/sorrow.png');
+
+INSERT INTO cards VALUES (38, 'sweetheart', '', '+', '../static/gypsy_card/sweetheart.png');
+
+INSERT INTO cards VALUES (39, 'thief', '', '+', '../static/gypsy_card/thief.png');
+
+INSERT INTO cards VALUES (40, 'thought', '', '+', '../static/gypsy_card/thought.png');
+
+INSERT INTO cards VALUES (41, 'visit', '', '+', '../static/gypsy_card/visit.png');
+
+INSERT INTO cards VALUES (42, 'waiting', '', '+', '../static/gypsy_card/waiting.png');
+
+INSERT INTO cards VALUES (43, 'wedding', '', '+', '../static/gypsy_card/wedding.png');
+
+INSERT INTO cards VALUES (44, 'widower', '', '+', '../static/gypsy_card/widower.png');
+
+INSERT INTO cards VALUES (45, 'wife', '', '+', '../static/gypsy_card/wife.png');
+
+INSERT INTO cards VALUES (46, 'young_woman', '', '+', '../static/gypsy_card/young_woman.png');
 
 
